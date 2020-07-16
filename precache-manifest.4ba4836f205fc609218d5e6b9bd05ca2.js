9c993e5daafb2aff4e869b1cdd1ba6295cb63d11
ricardogusi <ricardogusi@hotmail.com>
2020-07-16 11:36:55 -0300

    Updates

---
+++ precache-manifest.4ba4836f205fc609218d5e6b9bd05ca2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b26b7330e6ecf80b2828c51fa28ed2e5",
+    "url": "/recipesReact/index.html"
+  },
+  {
+    "revision": "9c74d27c4f81e7586cd6",
+    "url": "/recipesReact/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "819491350f83507104a0",
+    "url": "/recipesReact/static/css/main.26cd9126.chunk.css"
+  },
+  {
+    "revision": "9c74d27c4f81e7586cd6",
+    "url": "/recipesReact/static/js/2.7592cd4f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/recipesReact/static/js/2.7592cd4f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "819491350f83507104a0",
+    "url": "/recipesReact/static/js/main.d399b1ed.chunk.js"
+  },
+  {
+    "revision": "f1d710ff4c5e135811dd",
+    "url": "/recipesReact/static/js/runtime-main.2f387065.js"
+  }
+]);
